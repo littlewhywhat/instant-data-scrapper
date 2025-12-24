e7de14caff4bc03323598c816682091d6df7feb8
littlewhywhat <littlewhywhat@gmail.com>
2025-12-24 22:48:22 +0100

    cursor: add tesseract files to web_accessible_resources in manifest

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
   outDir: 'build',
   manifest: {
     permissions: ['activeTab', 'scripting'],
+    web_accessible_resources: [
+      {
+        resources: ['tesseract/*'],
+        matches: ['<all_urls>'],
+      },
+    ],
   },
   webExt: {
     disabled: true,
